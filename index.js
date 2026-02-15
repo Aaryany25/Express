5320ead41cc5c10969955394b4f6261b9af01fb4
Aaryan <aaryany2505@gmail.com>
2026-02-15 22:48:24 +0530

    Added More methods

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/contact',function(req,res){
 app.post('/tweet',function(req,res){
     res.status(201).end("This is Twwet Page Server ")
 })
+app.get('/tweet',function(req,res){
+    res.end("this is tweet page")
+})
+app.get('/get',function(req,res){
+res.end("this is get response")
+})
 app.listen(8000,function(){
     console.log("Server is ruuning on port 8000")
 })
